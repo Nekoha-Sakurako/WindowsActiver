6c7a072f10219049299d07eee1bd38102b7fa289
猫羽桜子 <88151433+Nekoha-Sakurako@users.noreply.github.com>
2025-11-28 07:12:06 +0800

    im trying to integrate slmgr to project

---
+++ WindowsActiver/WindowsActiver/Main.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class WinActFormMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        WinActLabel = New Label()
+        WinActExec = New Button()
+        WinActKeyList = New ComboBox()
+        WinActServerList = New ComboBox()
+        WinActKeyTips = New Label()
+        WinActServerTips = New Label()
+        WinActBar = New ProgressBar()
+        WinActUtilities = New ComboBox()
+        ContextMenuStrip1 = New ContextMenuStrip(components)
+        SuspendLayout()
+        ' 
+        ' WinActLabel
+        ' 
+        WinActLabel.AutoSize = True
+        WinActLabel.Font = New Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        WinActLabel.Location = New Point(62, 56)
+        WinActLabel.Name = "WinActLabel"
+        WinActLabel.Size = New Size(266, 45)
+        WinActLabel.TabIndex = 0
+        WinActLabel.Text = "WindowsActiver"
+        ' 
+        ' WinActExec
+        ' 
+        WinActExec.Location = New Point(62, 391)
+        WinActExec.Name = "WinActExec"
+        WinActExec.Size = New Size(75, 23)
+        WinActExec.TabIndex = 1
+        WinActExec.Text = "激活"
+        WinActExec.UseVisualStyleBackColor = True
+        ' 
+        ' WinActKeyList
+        ' 
+        WinActKeyList.FormattingEnabled = True
+        WinActKeyList.Location = New Point(52, 162)
+        WinActKeyList.Name = "WinActKeyList"
+        WinActKeyList.Size = New Size(288, 25)
+        WinActKeyList.TabIndex = 3
+        WinActKeyList.Text = "（请选择预设产品密钥或输入现有产品密钥）"
+        ' 
+        ' WinActServerList
+        ' 
+        WinActServerList.FormattingEnabled = True
+        WinActServerList.Location = New Point(52, 255)
+        WinActServerList.Name = "WinActServerList"
+        WinActServerList.Size = New Size(288, 25)
+        WinActServerList.TabIndex = 4
+        WinActServerList.Text = "（请选择预设KMS服务器或输入现有服务器地址）"
+        ' 
+        ' WinActKeyTips
+        ' 
+        WinActKeyTips.AutoSize = True
+        WinActKeyTips.Location = New Point(52, 142)
+        WinActKeyTips.Name = "WinActKeyTips"
+        WinActKeyTips.Size = New Size(140, 17)
+        WinActKeyTips.TabIndex = 5
+        WinActKeyTips.Text = "请在此处选择产品密钥："
+        ' 
+        ' WinActServerTips
+        ' 
+        WinActServerTips.AutoSize = True
+        WinActServerTips.Location = New Point(52, 235)
+        WinActServerTips.Name = "WinActServerTips"
+        WinActServerTips.Size = New Size(155, 17)
+        WinActServerTips.TabIndex = 6
+        WinActServerTips.Text = "请在此处选择KMS服务器："
+        ' 
+        ' WinActBar
+        ' 
+        WinActBar.Location = New Point(52, 319)
+        WinActBar.Name = "WinActBar"
+        WinActBar.Size = New Size(288, 23)
+        WinActBar.TabIndex = 7
+        ' 
+        ' WinActUtilities
+        ' 
+        WinActUtilities.FlatStyle = FlatStyle.System
+        WinActUtilities.Items.AddRange(New Object() {"备用激活方案…", "卸载产品密钥…", "取消已设置的KMS服务器…", "显示激活状态…", "查询到期时间…", "取消激活（危险功能！！！）…", "高级功能…", "调试功能…"})
+        WinActUtilities.Location = New Point(160, 389)
+        WinActUtilities.Name = "WinActUtilities"
+        WinActUtilities.Size = New Size(168, 25)
+        WinActUtilities.TabIndex = 8
+        WinActUtilities.Text = "其他功能"
+        ' 
+        ' ContextMenuStrip1
+        ' 
+        ContextMenuStrip1.Name = "ContextMenuStrip1"
+        ContextMenuStrip1.Size = New Size(181, 26)
+        ' 
+        ' WinActFormMain
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 17F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(383, 498)
+        Controls.Add(WinActUtilities)
+        Controls.Add(WinActBar)
+        Controls.Add(WinActServerTips)
+        Controls.Add(WinActKeyTips)
+        Controls.Add(WinActServerList)
+        Controls.Add(WinActKeyList)
+        Controls.Add(WinActExec)
+        Controls.Add(WinActLabel)
+        Name = "WinActFormMain"
+        Text = "WindowsActiver"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents WinActLabel As Label
+    Friend WithEvents WinActExec As Button
+    Friend WithEvents WinActKeyList As ComboBox
+    Friend WithEvents WinActServerList As ComboBox
+    Friend WithEvents WinActKeyTips As Label
+    Friend WithEvents WinActServerTips As Label
+    Friend WithEvents WinActBar As ProgressBar
+    Protected WithEvents WinActUtilities As ComboBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+
+End Class
